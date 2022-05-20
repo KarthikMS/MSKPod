b43f749173143d57fe7cae690018375f388e6271
Karthik <mskarthik6@gmail.com>
2022-05-20 20:55:37 +0100

    Added ColorProvider.

---
+++ ColorProvider.swift
@@ -0,0 +1,18 @@
+//
+//  ColorProvider.swift
+//  MSKColors
+//
+//  Created by Karthik Maharajan Skandarajah on 20/05/22.
+//
+
+import UIKit
+
+public class ColorProvider {
+    public static func getAwesomeColor() -> UIColor {
+        .red
+    }
+    
+    static func getPrivateColor() -> UIColor {
+        .blue
+    }
+}
